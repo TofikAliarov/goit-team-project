52b81f5d75c29f48b2d5d3d1070f4d1d0417d4c8
Tofik <smilokiller@mail.ru>
2020-07-12 17:39:00 +0300

    Updates

--- menu.15d766fc.js
+++ menu.15d766fc.js
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "19815" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55720" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
